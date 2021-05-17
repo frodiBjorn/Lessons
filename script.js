5bff49be933351f3103765a500bbfd321430647f
Ivan Fedotovsky <fedotovsky96@gmail.com>
2021-05-17 13:29:56 +0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert("hello world!")
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time  = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = prompt("Во сколько обойдется?");
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+
+
+alert("Ваш бюджет на один день - " + (appData.budget / 30));
+console.log(appData);
+
+
